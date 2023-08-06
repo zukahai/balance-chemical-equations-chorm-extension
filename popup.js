c5b9c3e8c0fba107c6fd9dbb9c997e3d55580d87
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2023-08-06 15:45:09 +0700

    done extension

--- popup.js
+++ popup.js
@@ -2,12 +2,46 @@ document.addEventListener('DOMContentLoaded', function() {
     const displayButton = document.getElementById('displayButton');
     const inputString = document.getElementById('inputString');
     const outputPthh = document.getElementById('outputPthh');
+    const outpoutElement = document.getElementById('element');
   
     displayButton.addEventListener('click', function() {
       const inputValue = inputString.value;
-      const result = pthh(inputValue)['text'];
-      console.log(result);
-      outputPthh.textContent = result;
+      const result = pthh(inputValue)
+      const resultText = result['text'];
+      const resultSuccess = result['success'];
+      outputPthh.textContent = resultText;
+      if (resultSuccess) {
+        outputPthh.style.color = 'green';
+      } else {
+        outputPthh.style.color = 'red';
+      }
+
+      outpoutElement.innerHTML = '';
+      let table = document.createElement('table');
+
+      if (resultSuccess) {
+        let tr = document.createElement('tr');
+        let th = document.createElement('th');
+        th.textContent = 'Element';
+        tr.appendChild(th);
+        let th2 = document.createElement('th');
+        th2.textContent = 'Quantity';
+        tr.appendChild(th2);
+        table.appendChild(tr);
+        
+        for (let el in result['element']) {
+          let tr = document.createElement('tr');
+          let td = document.createElement('td');
+          td.textContent = el;
+          tr.appendChild(td);
+          let td2 = document.createElement('td');
+          td2.textContent = result['element'][el];
+          tr.appendChild(td2);
+          table.appendChild(tr);
+        }
+      }
+
+      outpoutElement.appendChild(table);
     });
   });
   
