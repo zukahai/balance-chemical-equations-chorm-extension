51a3d390f2e20044a86123cc8e42d7a7b2b042e5
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2023-08-09 20:39:11 +0700

    update troll extension

--- extension-troll/config.js
+++ extension-troll/config.js
@@ -1,3 +1,10 @@
 const CONFIG = {
-    'imgPath': 'https://i0.wp.com/thatnhucuocsong.com.vn/wp-content/uploads/2022/09/anh-conan-dep-nhat-1.jpg?fitu003d857%2C933u0026sslu003d1',
+    'image' : {
+        'static': true,
+        'imgPath': 'https://i0.wp.com/thatnhucuocsong.com.vn/wp-content/uploads/2022/09/anh-conan-dep-nhat-1.jpg?fitu003d857%2C933u0026sslu003d1',
+    },
+    'title' : {
+        'static': true,
+        'title': 'Conan',
+    },
 }
--- extension-troll/content.js
+++ extension-troll/content.js
@@ -1,7 +1,20 @@
-$(document).ready(function() {
+if (CONFIG.image.static) {
+  $(document).ready(function() {
     $("img").attr("src", CONFIG.imgPath);
     $(window).on('scroll', function() {
       // Xử lý sự kiện cuộn ở đây
-      $("img").attr("src", CONFIG.imgPath);
+      $("img").attr("src", CONFIG.image.imgPath);
     });
-});
+  });
+}
+
+if (CONFIG.title.static) {
+  $(document).ready(function() {
+    // Đặt nội dung mới cho tất cả các tiêu đề h1, h2, ..., h6
+    $("h1, h2, h3, h4, h5, h6").html(CONFIG.title.title);
+    $(window).on('scroll', function() {
+      // Xử lý sự kiện cuộn ở đây
+      $("h1, h2, h3, h4, h5, h6").html(CONFIG.title.title);
+    });
+  });
+}
